a3ba667f5fc416389ea3e150de83462632f4c0a4
willithappen <willithappenyt@gmail.com>
2020-05-31 04:06:05 +1000

    Adjusted mass of bandages and added blood bag items

--- functions/eams_medical_items.hpp
+++ functions/eams_medical_items.hpp
@@ -2,35 +2,71 @@
 class CBA_MiscItem;\
 class CBA_MiscItem_ItemInfo;\
 class EAMS_ItemCore: CBA_MiscItem {};\
-class EAMS_BasicBandage: EAMS_ItemCore {\
+class EAMS_BloodBag: EAMS_ItemCore {\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
+	scope = 2;\
+	displayName = "Blood IV (1000ml)";\
+	descriptionShort = "Use to replace blood loss<br/>Usable on: Self, Others";\
+	model = "\A3\Structures_F_EPA\Items\Medical\BloodBag_F.p3d";\
+	picture = "\eams\data\icons\IVBag_ICO.paa";\
+	mass = 7;\
+	class ItemInfo: CBA_MiscItem_ItemInfo {\
+		mass = 7;\
+	};\
+};\
+class EAMS_BloodBag_500: EAMS_ItemCore {\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
+	scope = 2;\
+	displayName = "Blood IV (500ml)";\
+	descriptionShort = "Use to replace blood loss<br/>Usable on: Self, Others";\
+	model = "\A3\Structures_F_EPA\Items\Medical\BloodBag_F.p3d";\
+	picture = "\eams\data\icons\IVBag_ICO.paa";\
+	mass = 3.5;\
+	class ItemInfo: CBA_MiscItem_ItemInfo {\
+		mass = 3.5;\
+	};\
+};\
+class EAMS_BloodBag_250: EAMS_ItemCore {\
 	author = "Easy ArmA Medical System - willithappen";\
+	scope = 2;\
+	displayName = "Blood IV (250ml)";\
+	descriptionShort = "Use to replace blood loss<br/>Usable on: Self, Others";\
+	model = "\A3\Structures_F_EPA\Items\Medical\BloodBag_F.p3d";\
+	picture = "\eams\data\icons\IVBag_ICO.paa";\
+	mass = 1.75;\
+	class ItemInfo: CBA_MiscItem_ItemInfo {\
+		mass = 1.75;\
+	};\
+};\
+class EAMS_BasicBandage: EAMS_ItemCore {\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
 	scope = 2;\
 	displayName = "Basic Bandage";\
 	descriptionShort = "Use to treat wounds.<br/>Usable on: Self, Others";\
 	model = "\A3\Structures_F_EPA\Items\Medical\Bandage_F.p3d";\
 	picture = "\eams\data\icons\Bandage_Basic_ICO.paa";\
-	mass = 2;\
+	mass = 1.75;\
 	class ItemInfo: CBA_MiscItem_ItemInfo {\
-		mass = 2;\
+		mass = 1.75;\
 	};\
 };\
 class EAMS_BasicBandage_Half: EAMS_ItemCore {\
-	author = "Easy ArmA Medical System - willithappen";\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
 	scope = 1;\
 	displayName = "Basic Bandage (Half)";\
 	descriptionShort = "Use to treat wounds.<br/>Usable on: Self, Others";\
 	model = "\A3\Structures_F_EPA\Items\Medical\Bandage_F";\
 	picture = "\eams\data\icons\Bandage_Basic_ICO";\
-	mass = 1;\
+	mass = 0.85;\
 	class ItemInfo: CBA_MiscItem_ItemInfo {\
-		mass = 1;\
+		mass = 0.85;\
 	};\
 };\
 class EAMS_Epinephrine: EAMS_ItemCore {\
-	author = "Easy ArmA Medical System - willithappen";\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
 	scope = 2;\
 	displayName = "Epinephrine Autoinjector";\
-	descriptionShort = "Use to treat wounds.<br/>Usable on: Self, Others";\
+	descriptionShort = "Used to wake a patient up and revive<br/>Usable on: Self, Others";\
 	model = "a3\Props_F_Orange\Humanitarian\Garbage\MedicalGarbage_01_Injector_F";\
 	picture = "\eams\data\icons\Injector_Epinephrine_ICO";\
 	mass = 0.5;\
@@ -39,7 +75,7 @@ class EAMS_Epinephrine: EAMS_ItemCore {\
 	};\
 };\
 class EAMS_MedicBag: EAMS_ItemCore {\
-	author = "Easy ArmA Medical System - willithappen";\
+	author = "Easy ArmA Medical System - willithappen / Bohemia Interactive";\
 	scope = 2;\
 	displayName = "Medic Bag";\
 	descriptionShort = "Use to speed up healing of wounds<br/>Usable on: Self, Others";\
